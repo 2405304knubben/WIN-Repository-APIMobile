2a26ab1897684d26db9888f0a9b41b15ef4f1015
SemDuvan <24033737duvan@zuyd.nl>
2025-06-17 00:20:40 +0200

    Order page added Bestellingen zijn te zien (empty data) maar word niet automatisch geupdate dus dat word morgen gedaan

--- MauiApp1/OrdersPage.xaml.cs
+++ MauiApp1/OrdersPage.xaml.cs
@@ -1,9 +1,14 @@
+using MauiApp1.ViewModel;
+using MauiApp1.ApiService;
+
 namespace MauiApp1;
 
 public partial class OrdersPage : ContentPage
 {
-	public OrdersPage()
-	{
-		InitializeComponent();
-	}
+    public OrdersPage()
+    {
+        InitializeComponent();
+        BindingContext = new OrdersPageViewModel(
+            Application.Current.Handler.MauiContext.Services.GetService<ApiService.ApiService>());
+    }
 }
