291e43e49181d27148a612ab50e1f90201a463c3
SemDuvan <24033737duvan@zuyd.nl>
2025-06-24 13:29:00 +0200

    Map geaddd maar er word niks getoond?

--- MauiApp1/MVVM/ViewModel/DeliveryTrackingPageViewModel.cs
+++ MauiApp1/MVVM/ViewModel/DeliveryTrackingPageViewModel.cs
@@ -1,9 +1,12 @@
+using System.Globalization;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MauiApp1.ApiService;
 using MauiApp1.ModelAPI;
 using Microsoft.Maui.ApplicationModel;
-
+using Microsoft.Maui.Devices.Sensors;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace MauiApp1.MVVM.ViewModel
 {
@@ -18,27 +21,19 @@ namespace MauiApp1.MVVM.ViewModel
         [ObservableProperty]
         private string? statusMessage;
 
-        public DeliveryState? LastDeliveryState => Order?.DeliveryStates?.OrderByDescending(s => s.DateTime).FirstOrDefault();        public bool ShowStartButton => LastDeliveryState?.State == DeliveryStateEnum.Pending || LastDeliveryState == null;
-        public bool ShowCompleteButton => LastDeliveryState?.State == DeliveryStateEnum.InTransit;        private string GetStatusMessage(DeliveryStateEnum? state)
-        {
-            return state switch
-            {
-                DeliveryStateEnum.Pending => "Klik op 'Start bezorging' om te beginnen",
-                DeliveryStateEnum.InTransit => "Bezorging is onderweg. Klik op 'Rond bezorging af' wanneer afgeleverd.",
-                DeliveryStateEnum.Delivered => "Bezorging is afgerond",
-                _ => "Status onbekend"
-            };
-        }
+        [ObservableProperty]
+        private Location? customerLocation;
 
-        public void UpdateStatus()
-        {
-            StatusMessage = GetStatusMessage(LastDeliveryState?.State);
-        }
+        public DeliveryState? LastDeliveryState => Order?.DeliveryStates?.OrderByDescending(s => s.DateTime).FirstOrDefault();
+        public bool ShowStartButton => LastDeliveryState?.State == DeliveryStateEnum.Pending || LastDeliveryState == null;
+        public bool ShowCompleteButton => LastDeliveryState?.State == DeliveryStateEnum.InTransit;
 
         public DeliveryTrackingPageViewModel(ApiService.ApiService apiService)
         {
             _apiService = apiService;
-        }        [RelayCommand]
+        }
+
+        [RelayCommand]
         private async Task StartDelivery()
         {
             if (Order == null) return;
@@ -47,10 +42,9 @@ namespace MauiApp1.MVVM.ViewModel
             {
                 StatusMessage = "Bezorging wordt gestart...";
                 var result = await _apiService.StartDeliveryAsync(Order.Id);
-                
+
                 if (result?.Any() == true)
                 {
-                    // Update alleen de delivery states, niet de hele order
                     Order.DeliveryStates = result;
                     UpdateStatus();
                     UpdateProperties();
@@ -91,17 +85,62 @@ namespace MauiApp1.MVVM.ViewModel
             }
         }
 
-
         private void UpdateProperties()
         {
             OnPropertyChanged(nameof(LastDeliveryState));
             OnPropertyChanged(nameof(ShowStartButton));
             OnPropertyChanged(nameof(ShowCompleteButton));
+            OnPropertyChanged(nameof(MapImageUrl));
+        }
+
+        private string GetStatusMessage(DeliveryStateEnum? state)
+        {
+            return state switch
+            {
+                DeliveryStateEnum.Pending => "Klik op 'Start bezorging' om te beginnen",
+                DeliveryStateEnum.InTransit => "Bezorging is onderweg. Klik op 'Rond bezorging af' wanneer afgeleverd.",
+                DeliveryStateEnum.Delivered => "Bezorging is afgerond",
+                _ => "Status onbekend"
+            };
+        }
+
+        public void UpdateStatus()
+        {
+            StatusMessage = GetStatusMessage(LastDeliveryState?.State);
         }
 
         partial void OnOrderChanged(Order? value)
         {
             UpdateProperties();
+            UpdateStatus();
+
+            if (value?.Customer?.Address is not null)
+            {
+                Debug.WriteLine("Adres gevonden, locatie wordt ingesteld ,dummydata");
+                CustomerLocation = new Location(52.379189, 4.899431);
+                OnPropertyChanged(nameof(MapImageUrl));
+            }
+            else
+            {
+                Debug.WriteLine("Geen adres gevonden, locatie blijft null");
+            }
         }
+
+        public string? MapImageUrl
+        {
+            get
+            {
+                if (CustomerLocation is null)
+                    return null;
+
+                // Mapbox: longitude,latitude!
+                return string.Format(
+                    "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s-l+000({0},{1})/{0},{1},14,0/600x300?access_token=pk.eyJ1IjoibmllbHNjcmVtZXJzIiwiYSI6ImNtNHJsdjNxZzA2cWoya3BkcXp0M2l3N3EifQ.7g4Ms4TNd9ZJPD0EcDM_yw",
+                    CustomerLocation.Longitude.ToString(CultureInfo.InvariantCulture),
+                    CustomerLocation.Latitude.ToString(CultureInfo.InvariantCulture)
+                );
+            }
+        }
+
     }
 }
